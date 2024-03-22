e54901dceb4522c5fa6ae636d66903e53ed01635
Ye Nay Thway <yenaythway77@gmail.com>
2024-03-22 09:28:51 +0630

    fetch token

--- lib/Providers/login_provider.dart
+++ lib/Providers/login_provider.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:real_time_chatting/Constants/url_const.dart';
 
 class LoginProvider extends ChangeNotifier {
   bool isLogin = false;
