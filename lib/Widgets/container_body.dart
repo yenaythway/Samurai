8cce4476372a3783e1f70fb93e3d8f0cf6964b6c
Ye Nay Thway <yenaythway77@gmail.com>
2024-04-26 22:27:55 +0630

    add common widget

--- lib/Widgets/container_body.dart
+++ lib/Widgets/container_body.dart
@@ -1,1 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:real_time_chatting/Constants/colors.dart';
 
+class ContainerBody extends StatelessWidget {
+  final Widget child;
+  const ContainerBody({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      color: primaryColor,
+      child: Container(
+        decoration: const BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20), topRight: Radius.circular(20))),
+        child: child,
+      ),
+    );
+  }
+}
